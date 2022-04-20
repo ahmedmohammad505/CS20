5df6c316c71df6c822a764eee2838a3686fa283c
ahmedmohammad505 <ahmed.shakeel.2005@gmail.com>
2022-04-20 09:42:57 -0600

    C6

---
+++ Chapter6/src/C6CRT.java
@@ -0,0 +1,17 @@
+
+public class C6CRT {
+	/* Ahmed S Chapter 6 #2-7
+/* 
+2. Method declaration is the first line which contains the method name, access level, return type, and parameters. The method body contains the statements that implement it.
+3. Access modifier
+4.  Visibility
+5. 
+Var1 is declared in main(). Var 2 is declared in main() for statement. Var3 is declared in method1(). Var4 is declared in method1() for statement.
+6a. public static int getVovels(String x)
+6b. public static int extractDigit(int x)
+6c. public static String insertString(int y, String x)
+7a. By types, order and number of parameters
+7b. Yes, only if the compiler can differentiate the methods with parameters
+
+
+}
