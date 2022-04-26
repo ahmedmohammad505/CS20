1d53ed7108adcd9deccb438706c5657bd2f69e36
ahmedmohammad505 <ahmed.shakeel.2005@gmail.com>
2022-04-26 11:23:01 -0600

    ch9

--- Chapter7/src/Circle1of4.java
+++ Chapter7/src/Circle1of4.java
@@ -4,7 +4,14 @@ public class Circle1of4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+Circle1of4 spot = new Circle1of4 ();
+spot.Radius(3);
+System.out.println("Circle Circumference: + spot.circumference");
+		
+	}
 
+	private void Radius(int i) {
+		// TODO Auto-generated method stub
 		
 	}
 
