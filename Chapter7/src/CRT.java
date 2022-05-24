96082b68d218a58c3d22fd7bf7f9168c2a0e5ab5
ahmedmohammad505 <ahmed.shakeel.2005@gmail.com>
2022-05-24 11:22:39 -0600

    CRT

--- Chapter7/src/CRT.java
+++ Chapter7/src/CRT.java
@@ -2,9 +2,13 @@
 public class CRT 
 {
 	/*
-	
-	 
-	 
+1. The attributes that are common to all circles are a centre point and a radius. However, a circle is a shape thus it should also encapsulate attributes or interfaces that are common to all shapes, such as width(), height(), draw(), move(), fill(), outline() and so on.
+2. The name of the constructor must be the same as the name of the class 
+3. The public access modifier allows a code from outside or inside the class to access the class's methods and properties while the private modifier prevents access to a class's methods or properties from any code that is outside the class.	 
+4. YES its valid. If the constuctor for circle excepts a interger and has a public integer field called radius
+5a. Roo
+5b. public
+5c. public class
 	 
 	 
 	 
