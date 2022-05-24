e3907e6969d5f1f79a1c034cdfeda1ab5e327155
ahmedmohammad505 <ahmed.shakeel.2005@gmail.com>
2022-05-24 10:47:55 -0600

    Mysavings

---
+++ Chapter7/src/Mysavings.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Mysavings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int choice = 7;
+		Mysavings myPiggyBank = new Mysavings();
+		
+		while(choice != 0);	
+		
+		System.out.println("\nWould you like to:");
+		System.out.println("0. Quit");
+		System.out.println("1. Show total in bank");
+		System.out.println("2. Add a penny to the bank");
+		System.out.println("3. Add a nickel to the bank");
+		System.out.println("4. Add a dime to the bank");
+		System.out.println("5. Add a quarter to the bank");
+		System.out.println("6. Take money out of the bank");
+		System.out.println("\nPlease enter the corresponding number of yourchoice\n");
+		Scanner input = new Scanner(System.in);
+		
+		
+		
+	}
+
+}
