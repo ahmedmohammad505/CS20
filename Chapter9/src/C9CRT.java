d5a6163a538162040f6500b6adca4cb1b89603df
ahmedmohammad505 <ahmed.shakeel.2005@gmail.com>
2022-05-30 09:21:15 -0600

    C9

---
+++ Chapter9/src/C9CRT.java
@@ -0,0 +1,25 @@
+
+public class C9CRT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		1. The index value in the third element is 2
+		2.int[] quantities= new int[20]
+		3. double[] heights= {1.65, 2.15, 4.95}
+		4. for (int grades: grades){
+			system.out.println(grades); }
+		5a. 
+		Step 1) create a copy of the first array
+		Step 2) have the index values that are greater than or equal to the new data be increased by 1
+		Step 3) copy the new data to the second array
+		Step 4) copy the second array back in the original array
+		
+        6. Passing a single element: passes by value, you can modify a copy of the element
+        passing entire array: passes by a reference pointer, you can modify the elements in memory
+		
+		
+	}
+
+}
