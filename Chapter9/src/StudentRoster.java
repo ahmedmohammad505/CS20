007819c48ddf056afd5e5db786d08112df42dc21
ahmedmohammad505 <ahmed.shakeel.2005@gmail.com>
2022-06-01 09:44:42 -0600

    Final Student Roster

--- Chapter9/src/StudentRoster.java
+++ Chapter9/src/StudentRoster.java
@@ -1,14 +1,25 @@
-import java.beans.Statement;
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.Scanner;
 public class StudentRoster {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	
-	
+		Scanner input =new Scanner(System.in);
+		System.out.print("Enter number of students:");
+		int number_of_student=input.nextInt();
+		String[] student=new String[number_of_student];
+		input.nextLine();
 		
+		for(int x=0; x<number_of_student; x++)
+		{
+			System.out.print("Enter a student name:");
+			student[x]=input.nextLine();
+		}
+		
+		System.out.println("Student Roster");
+		for(int x=0; x<number_of_student; x++)
+		{
+			System.out.println(student[x]);
 	}
 
+}
 }
